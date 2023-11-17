b7d3baa079910926eb3bd34589bd0251dd83de27
Gustavo Carvalho <gustavocarvalho.gc@gmail.com>
2023-11-16 22:58:36 -0300

    update certificado

--- certificado/js/certificado.js
+++ certificado/js/certificado.js
@@ -1,4 +1,6 @@
 
+
+
 function redirecionar(){
     entrada = document.querySelector("#entrada").value;
 
@@ -8,12 +10,25 @@ function redirecionar(){
 
     p.appendChild(texto);
 
-    saida = document.querySelector("#saida");
+    /* saida = document.querySelector("#saida");
 
-    saida.appendChild(p);
+    saida.appendChild(p); */
 
     sair = document.querySelector(".nome_ra");
     sair.classList.add("ativado");
 
-    window.print();
+    certificado = document.getElementById("certificado");
+    certificado.style.display = "contents";  // Adicionada esta linha
+
+    setTimeout(function() {
+        window.print();
+    }, 100);
+}
+
+function atualizarNome() {
+    var entrada = document.querySelector("#entrada").value;
+    var nomeUsuario = document.querySelector(".nome_usuario");
+
+    // Atualiza o texto da div com a entrada do input
+    nomeUsuario.textContent = entrada;
 }
